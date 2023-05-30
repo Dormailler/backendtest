3c8be1270fbdb9ea44c482b7905ac8b287455037
Dormailler <101726788+Dormailler@users.noreply.github.com>
2023-05-30 17:32:35 +0900

    Update HelloController.java

--- test/src/main/java/com/example/demo/HelloController.java
+++ test/src/main/java/com/example/demo/HelloController.java
@@ -1,4 +1,4 @@
-	package com.example.demo;
+package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +10,7 @@ public class HelloController {
 	@RequestMapping("/helloboot")
 	public ModelAndView helloboot() {
 		ModelAndView mv = new ModelAndView();
-		mv.addObject("model","스프링부트를 시작합니다");
+		mv.addObject("model","스프링부트를 시작합니다(깃허브)");
 		mv.setViewName("hello");
 		return mv;
 	}
